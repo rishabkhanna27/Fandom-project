1465e957fc88bde69d171a80044e60d8dd5d9b24
rishab1999 <rishabkhanna27@gmail.com>
2019-12-09 11:10:21 +0530

    Updates

---
+++ precache-manifest.c2098834ce9342b7a5f6a12c9bcf0d4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3a94ac327175a37139bd8f0e92a57f",
+    "url": "/test-react-deploy/index.html"
+  },
+  {
+    "revision": "d67ecdb6fa9ed71e9e89",
+    "url": "/test-react-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1032da8c71350a6bb489",
+    "url": "/test-react-deploy/static/js/2.f223fb61.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-react-deploy/static/js/2.f223fb61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d67ecdb6fa9ed71e9e89",
+    "url": "/test-react-deploy/static/js/main.96571af2.chunk.js"
+  },
+  {
+    "revision": "3477ff93ba9e06081fd3",
+    "url": "/test-react-deploy/static/js/runtime-main.175ca114.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.f1b2cb16bbda1c4c540dd4ef94ac2bae.js"
+  "/test-react-deploy/precache-manifest.c2098834ce9342b7a5f6a12c9bcf0d4d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-react-deploy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
